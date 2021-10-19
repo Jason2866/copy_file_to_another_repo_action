a3188853c88661964f9f52c92892029874bd2f98
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-10-19 12:12:33 +0200

    undo pull

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,6 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
-  git pull --ff-only origin HEAD:$OUTPUT_BRANCH
   git push -u origin HEAD:$OUTPUT_BRANCH
 else
   echo "No changes detected"
