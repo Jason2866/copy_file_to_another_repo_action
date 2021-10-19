ce56a905f2877991a724f2c1b2f9aafb81696303
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-10-19 12:22:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
-  git pull --rebase origin HEAD:$OUTPUT_BRANCH
+  git pull --ff-only $INPUT_DESTINATION_BRANCH
   git push -u origin HEAD:$OUTPUT_BRANCH
 else
   echo "No changes detected"
