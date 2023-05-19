422c0c22547191abc892929d898d4d9a70ab96d8
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-05-19 13:36:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,6 @@ OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
 CLONE_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
-apt-get install -y git-lfs
 git config --global http.version HTTP/1.1
 git config --global http.postBuffer 157286400
 git config --global user.email "$INPUT_USER_EMAIL"
